101f7c368e8d90bc9b71651775f5e6ce90249027
sifat-arch <sifatullah1004@gmail.com>
2026-02-16 14:42:16 +0600

    added my initial changes to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <h1>Hello next.js</h1>
-     <p>adding developer2 Sifat Ullah</p>
-     <p>team members added</p>
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Hello next.js</h1>
+      <p>adding developer2 Sifat Ullah</p>
+      <p>team members added</p>
+      <div className="bg-orange-400 p-2 mt-5 rounded-md">
+        <h1 className="text-2xl font-bold">Hello guys</h1>
+      </div>
     </div>
   );
 }
