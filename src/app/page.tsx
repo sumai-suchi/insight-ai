75bc4ad2c55a9c494cb23094b79ba564e62243f5
sorkar5sourav <sorkar.s.sourav@gmail.com>
2026-02-18 11:32:02 +0600

    added homepage components for team members to work in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,22 @@
+import Footer from "@/components/Footer";
+import Features from "@/components/homepage/Features";
+import Hero from "@/components/homepage/Hero";
+import HowItWorks from "@/components/homepage/HowItWorks";
+import Testimonials from "@/components/homepage/Testimonials";
+import TrendingNews from "@/components/homepage/TrendingNews";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-around py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <h1 className="text-3xl font-bold">Welcome to Insight AI</h1>
-        <h2>this is hasan branch</h2>
-        <h3>test perpose</h3>
-        <h3>sumaiya here</h3>
-        <h3>ahadul islam here</h3>
+      <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-center gap-10 py-32 px-16 bg-white dark:bg-black sm:items-start">
+        
+        <Hero />
+        <Features />
+        <HowItWorks />
+        <TrendingNews />
+        <Testimonials />
+        <Footer />
       </main>
-    </div>
+    
   );
 }
