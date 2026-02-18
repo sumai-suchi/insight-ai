11625eab66bc785559e897d8808cbf741fcdaccb
Siddiqur Rahman <mdsqkn@gmail.com>
2026-02-19 02:06:25 +0600

    [Modified] the history section for Ai Editor

--- src/app/ai-editing/page.tsx
+++ src/app/ai-editing/page.tsx
@@ -1,12 +1,15 @@
 import HeroAi from "@/components/aiEditing/HeroAi";
 import NavAi from "@/components/aiEditing/NavAi";
+import RevisionHistory from "@/components/aiEditing/RevisionHistory";
 import React from "react";
 
 const aiEditing = () => {
   return (
     <div>
       <NavAi />
+
       <HeroAi />
+      <RevisionHistory />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import Navbar from "@/components/navbar";
 export default function Home() {
   return (
     <>
-      {" "}
       <Navbar />
       <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-center mx-auto gap-10 py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Hero />
