15420237882c026bc82fd2c84ef80b825e78f27d
sifat-arch <sifatullah1004@gmail.com>
2026-02-18 18:01:08 +0600

    feat: add trending news section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { getSession } from "@/lib/auth/auth"; // server-side helper
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,16 @@ import Hero from "@/components/homepage/Hero";
 import HowItWorks from "@/components/homepage/HowItWorks";
 import Testimonials from "@/components/homepage/Testimonials";
 import TrendingNews from "@/components/homepage/TrendingNews";
-import Image from "next/image";
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-center gap-10 py-32 px-16 bg-white dark:bg-black sm:items-start">
-        
-        <Hero />
-        <Features />
-        <HowItWorks />
-        <TrendingNews />
-        <Testimonials />
-        <Footer />
-      </main>
-    
+    <main className="flex min-h-screen flex-col items-center justify-center gap-10 py-32  bg-white dark:bg-black sm:items-start">
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <TrendingNews />
+      <Testimonials />
+      <Footer />
+    </main>
   );
 }
