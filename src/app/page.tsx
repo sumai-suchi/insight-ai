7c8399585ed208574b7d3f0eb226732de940ce1e
hasan9218 <hasanbhuiyan503@gmail.com>
2026-02-26 10:09:14 +0600

    added testmonial section and modify some section width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,20 @@ import HowItWorks from "@/components/homepage/HowItWorks";
 import OutputPreview from "@/components/homepage/OutputPreview";
 import AiDemoChat from "@/components/homepage/AiDemoChat";
 import TrendingNews from "@/components/homepage/TrendingNews";
+import Testimonials from "@/components/homepage/Testimonials";
 
 export default function Home() {
   return (
     <>
       {/* <Navbar /> */}
-      <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-center mx-auto gap-10 py-32 px-16 bg-white dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-center mx-auto gap-10 py-32 bg-white dark:bg-black">
         <Hero />
         <AiDemoChat />
         <Features />
         <HowItWorks />
         <TrendingNews />
         <OutputPreview />
+        <Testimonials />
       </main>
       {/* <Footer /> */}
     </>
