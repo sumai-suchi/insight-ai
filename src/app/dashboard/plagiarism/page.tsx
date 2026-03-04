e347107f152f7a2731673c89c83f358cc6eb60b6
sorkar5sourav <sorkar.s.sourav@gmail.com>
2026-03-04 13:10:43 +0600

    inserted basic design

--- src/app/dashboard/plagiarism/page.tsx
+++ src/app/dashboard/plagiarism/page.tsx
@@ -1,13 +1,17 @@
+import Tiptap from "@/components/text-editor";
 import React from "react";
 
 export default function Page() {
   return (
-    <div className="flex flex-col lg:flex-row gap-6 p-10">
-      <div className="h-full w-full lg:w-1/2 border bg-white rounded-lg p-6">
+    <div className="flex flex-col lg:flex-row">
+      <div className="h-full w-full lg:w-2/3  border-r border-gray-200 pt-6 px-6">
         <h2 className="text-2xl font-bold">Plagiarism Checker</h2>
-        <p className="text-sm text-gray-500">Check your text for plagiarism</p>
+        <p className="text-sm text-gray-500 mb-4">
+          Check your text for plagiarism
+        </p>
+        <Tiptap />
       </div>
-      <div className="h-full w-full lg:w-1/2 border bg-white rounded-lg p-6">
+      <div className="h-screen w-full lg:w-1/3 border p-6 bg-white">
         <h2 className="text-2xl font-bold">Plagiarism Checker</h2>
         <p className="text-sm text-gray-500">Check your text for plagiarism</p>
         <div className="flex flex-col gap-2">
